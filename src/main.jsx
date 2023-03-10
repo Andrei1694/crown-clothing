37cc82addc798a9d8fa2c0d393264fa0f828ec45
Andrei Stanciu <andrei4.stanciu@gmail.com>
2023-03-10 16:06:02 +0200

    setting categories data in redux works, finished 159

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
-import { UserProvider } from "./contexts/user.context";
 import { CartProvider } from "./contexts/cart.context";
-import { CategoriesProvider } from "./contexts/categories.context";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
@@ -13,13 +11,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        {/* <UserProvider> */}
-        {/* <CategoriesProvider> */}
         <CartProvider>
           <App />
         </CartProvider>
-        {/* </CategoriesProvider> */}
-        {/* </UserProvider> */}
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
