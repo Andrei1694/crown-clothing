ab2af7b33d03ec2010b6e7fc171c7fd250e98440
Andrei Stanciu <andrei4.stanciu@gmail.com>
2023-03-10 20:13:49 +0200

    added reselect

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,20 @@
-export const selectCategoriesMap = (state) => state.categories.categories
-    .reduce((acc, category) => {
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+
+)
+
+export const selectCategoriesMap = createSelector([selectCategories], (categories) => {
+    return categories.reduce((acc, category) => {
         console.log(category)
         const { title, items } = category;
         acc[title.toLowerCase()] = items;
         return acc;
     }, {});
+})
+
 
